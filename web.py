a30b980444b21526ae5d0ad82a9a7fe3ea827716
Andrey-Gubs <andrej.gubskiy@gmail.com>
2025-09-19 09:07:46 +0000

    IP2.2

--- web.py
+++ web.py
@@ -9,4 +9,4 @@ def index():
     user_ip = request.remote_addr
     return f"Ваш IP-адрес: {user_ip}"
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5003)

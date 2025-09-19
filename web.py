00fb0fc582bfa5f2abfcfc978094661685595d44
Andrey-Gubs <andrej.gubskiy@gmail.com>
2025-09-19 08:42:42 +0000

    IP addres2.0

--- web.py
+++ web.py
@@ -1,6 +1,5 @@
 
-from flask import Flask, render_template
-from flask import Flask, request
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
@@ -11,8 +10,8 @@ def index():
 
     current_time = now.strftime("%d/%m/%y %H:%M:%S")
     return current_time
+@app.route('/')
 def addres():
-    user_ip = request.remote_addr
-    return f"Ваш IP-адрес: {user_ip}"
+    return 'IP: '+ request.remote_addr
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5003)
